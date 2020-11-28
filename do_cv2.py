17d8f5019d7ee4512542ec10770f28c5e50956c6
inrixx <2312441059@qq.com>
2020-11-28 14:47:02 +0800

    Add files via upload

--- do_cv2.py
+++ do_cv2.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import cv2 as cv
+import random
 
 #图片旋转
 def rotate(image, angle, center=None, scale=1.0):
@@ -16,7 +17,11 @@ def rotate(image, angle, center=None, scale=1.0):
 def affine(image):
 	(cols,rows) = image.shape[:2]
 	pts1 = np.float32([[0,0], [cols-1,0], [0, rows-1], [cols-1,rows-1]])
-	pts2 = np.float32([[99,102], [cols-1,91], [50, rows-1], [cols-50,rows-50]])
+	# pts2 = np.float32([[99,102], [cols-1,91], [50, rows-1], [cols-50,rows-50]])
+	pts2 = np.float32([[random.randint(1,90), random.randint(1,90)], 
+		               [cols-random.randint(1,90), random.randint(1,90)], 
+		               [random.randint(1,90), rows-random.randint(1,90)], 
+		               [cols-random.randint(1,90),rows-random.randint(1,90)]])
 	M = cv.getPerspectiveTransform(pts1, pts2)
 	affined = cv.warpPerspective(image, M, (rows, cols))
 	return affined
@@ -26,17 +31,27 @@ def affine(image):
 if __name__ == '__main__':
 	image = "./pic/ex.png"
 	img = cv.imread(image, 0)
-	cv.imshow("ii", img)
-
 
 	# rotated = rotate(img, 70)
 	# cv.imshow("70", rotated)
 
-	affined = affine(img)
-	cv.imshow("2",affined)
+	# affined = affine(img)
+	# cv.imshow("2",affined)
+	# cv.imwrite("/home/end/pic_e/ex_1000.png",affined)
 
-	
+#旋转变换360张
+	# for i in range(260):
+	# 	j = i+400
+	# 	print(j)
+	# 	angle = random.randint(1,359)
+	# 	rotated = rotate(img,angle)
+	# 	cv.imwrite("/home/end/pic_e/ex_"+str(j)+".png",rotated)
 
+#仿射变换360张
+	for i in range(260):
+		j = i+400
+		print(j)
+		affined = affine(img)
+		cv.imwrite("/home/end/pic_e/ex_"+str(j)+".png",affined)
 
-	cv.waitKey(0)
-	cv.destroyAllWindows()
+	print("finish")
